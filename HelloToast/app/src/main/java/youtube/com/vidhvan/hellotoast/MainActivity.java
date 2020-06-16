cd171592af5ea44a7e6feb4a7315f9305e3cbaf8
Pavan kumar reddy <pavankumart46@gmail.com>
2020-06-16 12:05:20 +0530

    Hello Toast Completed App Added

--- HelloToast/app/src/main/java/youtube/com/vidhvan/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/youtube/com/vidhvan/hellotoast/MainActivity.java
@@ -3,12 +3,35 @@ package youtube.com.vidhvan.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mCount;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCount = findViewById(R.id.count_tv);
+    }
+
+    // The following method will be invoked when the toast button is clicked
+    public void showToast(View view)
+    {
+        Toast.makeText(this, "APSSDC & SVEC", Toast.LENGTH_SHORT).show();
+    }
+
+    /* This method will be called
+    * when the count button on the activity_main.xml
+    * is clicked*/
+    public void showCount(View view)
+    {
+        count++;
+        mCount.setText(String.valueOf(count));
     }
 }
