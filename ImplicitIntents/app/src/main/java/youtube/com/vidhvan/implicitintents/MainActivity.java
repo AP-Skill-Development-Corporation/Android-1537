098bfbe461a96edbbdad8c71c0cee0dcb20536cf
Pavan kumar reddy <pavankumart46@gmail.com>
2020-06-18 10:32:51 +0530

    Google Maps Added

--- ImplicitIntents/app/src/main/java/youtube/com/vidhvan/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/youtube/com/vidhvan/implicitintents/MainActivity.java
@@ -3,6 +3,7 @@ package youtube.com.vidhvan.implicitintents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.view.View;
@@ -25,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
         countdown_intent.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
 
         startActivity(countdown_intent);
+    }
+
+    public void openGMaps(View view)
+    {
+        EditText editText = findViewById(R.id.openmap);
+        String address = editText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q="+address));
+
+        startActivity(intent);
+    }
+
+    public void openMapslatlong(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q="+16.482759+","+80.607697+"(PAVAN)"));
+        startActivity(intent);
     }
 }
