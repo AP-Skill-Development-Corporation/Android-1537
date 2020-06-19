bd6de60ab04beaa7a6ce08de38793eaca5539906
Pavan kumar reddy <pavankumart46@gmail.com>
2020-06-19 11:01:20 +0530

    RadioButtons code is added

--- UserInputControls/app/src/main/java/youtube/com/vidhvan/userinputcontrols/MainActivity.java
+++ UserInputControls/app/src/main/java/youtube/com/vidhvan/userinputcontrols/MainActivity.java
@@ -3,12 +3,30 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup gender;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        gender = findViewById(R.id.gender);
+        
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int id = group.getCheckedRadioButtonId();
+                if(R.id.male == id){
+                    Toast.makeText(MainActivity.this, "MALE IS SELECTED", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "FEMALE IS SELECTED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
