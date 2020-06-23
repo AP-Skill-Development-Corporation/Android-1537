8643b126dd8b405650b6645a496efe6cc47e02e2
Pavan kumar reddy <pavankumart46@gmail.com>
2020-06-23 12:26:30 +0530

    COVID19 UPDATES

--- CoronaUpdates/app/src/main/java/youtube/com/vidhvan/coronaupdates/MainActivity.java
+++ CoronaUpdates/app/src/main/java/youtube/com/vidhvan/coronaupdates/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +22,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView result;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         result = findViewById(R.id.data);
+        progressBar = findViewById(R.id.progressbar);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(ScalarsConverterFactory.create())
@@ -34,12 +42,29 @@ public class MainActivity extends AppCompatActivity {
         str.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
+                progressBar.setVisibility(View.GONE);
                 String data = response.body();
-                result.setText(data);
+                try
+                {
+                    JSONArray array = new JSONArray(data);
+                    result.setText("");
+                    for(int i = array.length()-1; i>=0 ;i--){
+                        JSONObject object = array.getJSONObject(i);
+                        int confirmed_cases = object.getInt("Confirmed");
+                        String date = object.getString("Date");
+                        result.append("Date: "+date.substring(0,10)+" Confirmed: "+confirmed_cases+"\n\n");
+                    }
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
                 Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                 Log.i("MAIN", "onFailure: "+t.getMessage());
             }
